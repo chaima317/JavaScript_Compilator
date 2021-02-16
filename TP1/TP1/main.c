47b4bb7e64f3e9e729a6607a16132ebd6c745807
Chaima Abdellali <chaimaabdellali@macbook-air-de-chaima.home>
2021-02-16 11:30:49 +0100

    mon premier parseur

---
+++ TP1/TP1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TP1
+//
+//  Created by Chaima Abdellali on 09/02/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
